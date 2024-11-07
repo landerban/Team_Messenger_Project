912ea7bf4c2f9dacae056e44f6c141e0cdce8161
leapin-lizards <wonjaechoi2@gmail.com>
2024-11-07 23:46:20 +0900

    added login

--- customuser/views.py
+++ customuser/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from .models import CustomUser
+from django.contrib.auth import login,logout
+from .forms import UserRegistrationForm
 
-# Create your views here.
+def user_register(request):
+    if request.method=="POST":
+        form=UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("users:login")
+    else:
+        form=UserRegistrationForm()
+    return render(request,'register.html',{'form':form})
+
+def user_login(request):
+    if request.method=="POST":
+        form=AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request,form.get_user())
+            if "next" in request.POST:
+                return redirect(request.POST.get('next'))
+            return redirect("/")
+    else:
+
+        form=AuthenticationForm()
+    return render(request,'login.html',{'form':form})
+
+def user_logout(request):
+    if request.method=="POST":
+        logout(request)
+        return redirect("/")
+def user_profile(request,userid):
+    user=CustomUser.objects.get(userid=userid)
+    return render(request,'profile.html',{'user':user})
